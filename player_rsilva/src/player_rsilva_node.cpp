409a902684fcfd37b5bd21d3c6aa2d4d3ecfba8e
Ricardo Luís Silva <ricardolfsilva@ua.pt>
2018-03-01 16:06:18 +0000

    Added a ros package after working

--- player_rsilva/src/player_rsilva_node.cpp
+++ player_rsilva/src/player_rsilva_node.cpp
@@ -1,60 +1,111 @@
+#include <boost/shared_ptr.hpp>
 #include <iostream>
+#include <vector>
 
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+
+#include <rws2018_libs/team.h>
+#include <sstream>
+
+using namespace std;
+
+namespace rws_rsilva
+{
 class Player
 {
-    public:
+public:
+  // Constructor with the same name as the class
+  Player(string name)
+  {
+    this->name = name;
+  }
 
-    //Constructor with the same name as the class
-    Player(std::string name) { this->name = name; }
-    
-    int setTeamName(int team_index = 0 /*default value*/)
+  int setTeamName(int team_index = 0 /*default value*/)
+  {
+    switch (team_index)
     {
-        switch (team_index)
-        {
-            case 0: 
-                return setTeamName("red");
-                break;
-            case 1: 
-                return setTeamName("green");
-                break;
-            case 2: 
-                return setTeamName("blue");  
-                break;
-            default: 
-                std::cout << "wrong team index given. Cannot set team" << std::endl;
-                break;
-        }
-     }                                                                                                                                                      //Set team name, if given a correct team name (accessor)
-    int setTeamName(std::string team)
-    {
-        if (team =="red" || team =="green" || team =="blue")
-        {
-            this->team = team;
-            return 1;
-        }
-        else                                                                        {
-            std::cout << "cannot set team name to " << team << std::endl;
-            return 0;
-        }
+      case 0:
+        return setTeamName("red");
+        break;
+      case 1:
+        return setTeamName("green");
+        break;
+      case 2:
+        return setTeamName("blue");
+        break;
+      default:
+        cout << "wrong team index given. Cannot set team" << endl;
+        break;
     }
-    
-    //Gets team name (acessor)
-    std::string getTeamName(void) {return team;}
+  }
 
-    std::string name; //A public atribute
+  // Set team name, if given a correct team name (accessor)
+  int setTeamName(string team)
+  {
+    if (team == "red" || team == "green" || team == "blue")
+    {
+      this->team = team;
+      return 1;
+    }
+    else
+    {
+      this->team = "no team";
+      cout << "cannot set team name to " << team << endl;
+      return 0;
+    }
+  }
 
-    private:
-    std::string team;
+  // Gets team name (accessor)
+  string getTeam(void)
+  {
+    return team;
+  }
+
+  string name;  // A public atribute
+
+private:
+  string team;
 };
 
+class MyPlayer : public Player
+{
+public:
+  boost::shared_ptr<Team> red_team;
+  boost::shared_ptr<Team> green_team;
+  boost::shared_ptr<Team> blue_team;
 
+  MyPlayer(string argin_name, string argin_team) : Player(argin_name)
+  {
+    red_team = boost::shared_ptr<Team>(new Team("red"));
+    green_team = boost::shared_ptr<Team>(new Team("green"));
+    blue_team = boost::shared_ptr<Team>(new Team("blue"));
 
-int main()
-{    
-    //Creating an instance of class Player
-    Player player("rsilva");
-    player.setTeamName("red");
-    player.setTeamName(2);
-    std::cout << "player.name is" << player.name << std::endl;
-    std::cout << "team is" << player.getTeamName() << std::endl;                                                      
+    setTeamName(argin_team);
+
+    PrintReport();
+  }
+
+  void PrintReport()
+  {
+    cout << "My name is " << name << " and my team is " << getTeam() << endl;
+  }
+};
+}
+
+int main(int argc, char** argv)
+{
+  // Creating an instance of class Player
+  ros::init(argc, argv, "rsilva");
+
+  ros::NodeHandle n;
+
+  rws_rsilva::MyPlayer my_player("rsilva", "green");
+
+  if (my_player.red_team->playerBelongsToTeam("rsilva"))
+  {
+    cout << "o ricardo esta na equipa certa" << endl;
+  };
+
+  ros::spin();
 }
